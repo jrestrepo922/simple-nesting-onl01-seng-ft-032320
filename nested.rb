ce54474356638dcbb53fbdf59a5b9b237203a73b
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-28 12:48:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -115,6 +115,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     
      
 end
 
@@ -134,5 +136,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  
+  programmer_hash[:dennis_ritchie]
 end
